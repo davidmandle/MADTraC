e4297230f53b8d20fd4b31921e60b87e2e8c6d2a
AUVLAB User (pod) <dcsl@princeton.edu>
2012-08-07 18:05:53 -0400

    Modified waypoint_controller to obey new MT_AgentStates specs

--- samples/WaypointController/src/waypoint_controller.cc
+++ samples/WaypointController/src/waypoint_controller.cc
@@ -1,7 +1,6 @@
 #include "MT_Core.h"
 #include "MT_Tracking.h"
 #include <boost/lexical_cast.hpp>
-#include <boost/bind.hpp>
 #include <iostream>
 
 class WaypointController {
@@ -24,9 +23,9 @@ private:
     for (int i = 0; i < agent_states.agent_state_size(); i++) {
       if (agent_states.agent_state(i).has_tracked_agent_state() &&
   	  agent_states.agent_state(i).has_generic_agent_control() &&
-	  (agent_states.agent_state(i).generic_agent_control().control_mode() == MT_GenericAgentControl::MT_WAYPOINT_CONTROL)) {
+	  agent_states.agent_state(i).generic_agent_control().has_waypoint()) {
 	MT_GenericAgentControl generic_agent_control = agent_states.agent_state(i).generic_agent_control();
-	generic_agent_control.set_control_mode(MT_GenericAgentControl::MT_KINEMATICS_CONTROL);
+
 	MT_Kinematics* kinematics = agent_states.mutable_agent_state(i)->mutable_generic_agent_control()->mutable_kinematics();
 
 	MT_Waypoint waypoint = generic_agent_control.waypoint();
@@ -38,6 +37,7 @@ private:
 
 	//std::cerr << "waypoint x: " << waypoint.x() << std::endl;
 	//std::cerr << "waypoint y: " << waypoint.y() << std::endl;
+	std::cerr << "tracked z" << tracked_agent_state.z();
 
 	double dth = atan2(dy, dx) - tracked_agent_state.heading();
 	dth = atan2(sin(dth), cos(dth));
@@ -47,31 +47,31 @@ private:
 	kinematics->set_z_dot(0);
 	kinematics->set_omega(0);
 
-	double m_dDist = 5.0;
-	double m_dMaxSpeed = 15.0;
-	double m_dTurningGain = 25.0;
+	const double DIST = 0.3;
+	const double MAX_SPEED = 0.35;
+	const double TURNING_GAIN = 40.0;
+	const double VERTICAL_GAIN = 10;
 
-	if(d > 1.0*m_dDist)
+	if(d > 1.0*DIST)
 	  {
-	    kinematics->set_speed(m_dMaxSpeed);
+	    kinematics->set_speed(MAX_SPEED);
 	  }
 	else
 	  {
-	    kinematics->set_speed(m_dMaxSpeed*1.0*(d/m_dDist)*fabs(cos(dth)));
+	    kinematics->set_speed(MAX_SPEED*1.0*(d/DIST)*fabs(cos(dth)));
 	  }
-	kinematics->set_omega(-m_dTurningGain*sin(dth));
+	kinematics->set_omega(TURNING_GAIN*sin(dth));
 	if(dth > 1.57)
 	  {
-	    kinematics->set_omega(-m_dTurningGain);
+	    kinematics->set_omega(TURNING_GAIN);
 	  }
 	if(dth < -1.57)
 	  {
-	    kinematics->set_omega(m_dTurningGain);
+	    kinematics->set_omega(-TURNING_GAIN);
 	  }
-
-	agent_states.mutable_agent_state(i)->mutable_generic_agent_control()->clear_waypoint();
+	kinematics->set_z_dot(VERTICAL_GAIN * dz);
 	
-	//std::cerr << "dx = " << dx <<", dy = " << dy << ", dth = " << dth << ", dz = " << dz << std::endl;
+	//std::cerr << "d = " << d << ", dx = " << dx <<", dy = " << dy << ", dth = " << dth << ", dz = " << dz << std::endl;
 	//std::cerr << "Control out: speed " << agent_states.agent_state(i).generic_agent_control().kinematics().speed() << ", vert " << agent_states.agent_state(i).generic_agent_control().kinematics().z_dot() << ", steer " << agent_states.agent_state(i).generic_agent_control().kinematics().omega() << std::endl;
       }	    
   }
